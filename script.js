8bee2a1f0ee2df1cc4b6c1b3ff9ac724476f440f
Mateus Felipe Gonçalves <mateusfg7@localhost.localdomain>
2019-08-10 10:34:42 -0300

    add: raiz, potencia

--- script.js
+++ script.js
@@ -1,38 +1,63 @@
 function executar(){
 	
 	var numero = parseInt(document.getElementById('num').value);
+	
 	var soma = document.getElementById('soma');
 	var subtracao = document.getElementById('subtracao');
 	var multiplicacao = document.getElementById('multiplicacao');
 	var divisao = document.getElementById('divisao');
-	var tabuadaSom = '';
-	var tabuadaSub = '';
-	var tabuadaMult = '';
-	var tabuadaDiv = '';
+	var potencia = document.getElementById('potencia');
+	var raiz = document.getElementById('raiz');
 	
+	var tabuadaSoma = '';
+	var tabuadaSubtracao = '';
+	var tabuadaMultiplicacao = '';
+	var tabuadaDivisao = '';
+	var tabuadaPotencia = '';
+	var tabuadaRaiz = '';
+	
+
+	//soma
 	for (let index = 0; index < 10; index++) {
 		
-		tabuadaSom += `${numero} + ${index} = ${numero + index}<br>`;
-  		soma.innerHTML = tabuadaSom;
+		tabuadaSoma += `${numero} + ${index} = ${numero + index}<br>`;
+  		soma.innerHTML = tabuadaSoma;
 		
 	}
+	//subtração
 	for (let index = 0; index < 10; index++) {
 		
-		tabuadaSub += `${numero} - ${index} = ${numero - index}<br>`;
-  		subtracao.innerHTML = tabuadaSub;
+		tabuadaSubtracao += `${numero} - ${index} = ${numero - index}<br>`;
+  		subtracao.innerHTML = tabuadaSubtracao;
 		
 	}
+	//multiplicação
 	for (let index = 0; index < 10; index++) {
 		
-		tabuadaMult += `${numero} x ${index} = ${numero * index}<br>`;
-  		multiplicacao.innerHTML = tabuadaMult;
+		tabuadaMultiplicacao += `${numero} x ${index} = ${numero * index}<br>`;
+  		multiplicacao.innerHTML = tabuadaMultiplicacao;
 		
 	}
+	//divisão
 	for (let index = 0; index < 10; index++) {
 		
-		tabuadaDiv += `${numero} / ${index} = ${numero / index}<br>`;
-  		divisao.innerHTML = tabuadaDiv;
+		tabuadaDivisao += `${numero} / ${index} = ${numero / index}<br>`;
+  		divisao.innerHTML = tabuadaDivisao;
 		
+	}
+	//potencia
+	for (let index = 0; index < 10; index++) {
+		
+		tabuadaPotencia += `${numero} ^ ${index} = ${Math.pow(numero, index)}<br>`;
+  		potencia.innerHTML = tabuadaPotencia;
+		
+	}
+	//raiz
+	for (let index = 0; index < 10; index++) {
+		
+		tabuadaRaiz += `raiz ${index} de ${numero} = ${Math.pow(numero, 1.0 / index)}<br>`;
+  		raiz.innerHTML = tabuadaRaiz;
+		Math.sqrt()
 	}
 	
 }
